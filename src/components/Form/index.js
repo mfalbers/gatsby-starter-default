c02890593bc9c2835761afce200ff7495cb07bdb
Matt Albers <albers@gmail.com>
2020-04-18 12:12:51 -0700

    Fixed registration form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,39 +1,39 @@
 import React from "react"
-import styles from "./form.module.css"
+// import styles from "./form.module.css"
 import { navigate } from "@reach/router"
 import Button from 'react-bootstrap/Button';
 
 export default ({ handleSubmit, handleUpdate }) => (
   <form
-    className={styles.form}
+    className="form"
     method="post"
     onSubmit={event => {
       handleSubmit(event)
       navigate(`/app/profile`)
     }}
   >
-    <p className={styles[`form__instructions`]}>
+    <p className="form__instructions">
       To start a game or see your coach's profile. Please log in.
     </p>
-    <label className={styles[`form__label`]}>
+    <label className="form__label">
       Username
       <input
-        className={styles[`form__input`]}
+        className="form__input"
         type="text"
         name="username"
         onChange={handleUpdate}
       />
     </label>
-    <label className={styles[`form__label`]}>
+    <label className="form__label">
       Password
       <input
-        className={styles[`form__input`]}
+        className="form__input"
         type="password"
         name="password"
         onChange={handleUpdate}
       />
     </label>
-    {/* <input className={styles[`form__button`]} type="submit" value="Log In" /> */}
+    {/* <input className="form__button" type="submit" value="Log In" /> */}
     <Button type="submit">Log In</Button>
   </form>
 )
--- src/components/FormTextInputField/index.js
+++ src/components/FormTextInputField/index.js
@@ -6,13 +6,12 @@ const FormTextInputField = ({ label, ...props }) => {
     // which we can spread on <input> and also replace ErrorMessage entirely.
     const [field, meta] = useField(props);
     return (
-        <>
-            <label htmlFor={props.id || props.name} className="form__label">{label}</label>
+        <label htmlFor={props.id || props.name} className="form__label">{label}
             <input className="form__input" {...field} {...props} />
             {meta.touched && meta.error ? (
                 <div className="form__error">{meta.error}</div>
             ) : null}
-        </>
+        </label>
     );
 };
 
